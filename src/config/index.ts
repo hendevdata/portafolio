1115057354a2948e69c80af7a70b2d6dbc6f53db
Arkosi <arkosi@Arkosis-MacBook-Air.local>
2026-01-27 19:30:19 -0500

    portafolio update jan 27

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,97 +1,102 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
-  author: "Alejandro Múnez Cuntez",
+  title: "Henry Larreal— Web Developer and Data Enthusiast",
+  author: "Henry Larreal",
   description:
-    "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
+    "I specialize in Web Development, Data Science and Data Analysis. In love with this ones HTML, CSS, Javascript, PHP, Python, R, SQL and all the things related to data.",
   lang: "en",
-  siteLogo: "/alejandro-small.jpg",
+  siteLogo: "/henry.png",
   navLinks: [
     { text: "Experience", href: "#experience" },
     { text: "Projects", href: "#projects" },
     { text: "About", href: "#about" },
   ],
   socialLinks: [
-    { text: "Twitter", href: "https://github.com/immois/astro-zen" },
-    { text: "LinkedIn", href: "https://github.com/immois/astro-zen" },
-    { text: "Github", href: "https://github.com/immois/astro-zen" },
-    { text: "Youtube", href: "https://github.com/immois/astro-zen" },
-    { text: "Dribbble", href: "https://github.com/immois/astro-zen" },
+    //{ text: "Twitter", href: "https://github.com/immois/astro-zen" },
+    { text: "LinkedIn", href: "https://www.linkedin.com/in/henry-larreal-carrera/" },
+    { text: "Github", href: "https://github.com/hendevdata/hendevdata" },
+    //{ text: "Youtube", href: "https://github.com/immois/astro-zen" },
+    //{ text: "Dribbble", href: "https://github.com/immois/astro-zen" },
   ],
-  socialImage: "/zen-og.png",
+  socialImage: "/henry.png",
   canonicalURL: "https://astro-zen.vercel.app",
 };
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Alejandro Múnez",
-    specialty: "Mobile & Web Developer",
+    name: "Henry Larreal",
+    specialty: "Web Developer and Data Enthusiast",
     summary:
-      "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
-    email: "example@email.com",
+      "Developer based in Bogota, Colombia. I specialize in Web Development, Data Science and Data Analysis. In love with this ones HTML, CSS, Javascript, PHP, Python, R, SQL and all the things related to data.",
+    email: "henrylarreal27@gmail.com",
   },
   experience: [
     {
-      company: "Zalmart",
-      position: "Lead Android Developer",
-      startDate: "May 2018",
-      endDate: "Sept 2020",
+      company: "PMO ADVISORY",
+      position: "Web Publisher / Web Content Specialits",
+      startDate: "May 2023",
+      endDate: "Current",
       summary: [
-        "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
-        "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
-        "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
+        " Installed required plugins and additional coding to fit final design and functionality requirement",
+        " Converted design assets into component-based HTML, CSS and JavaScript.",
+        " Migrated existing websites to WordPress platform without loss of data or functionality, streamlining content updates for clients.",
+        " Coded websites using HTML, CSS, JavaScript, and jQuery languages..",
+        " Enhanced user experience by implementing responsive web design and optimizing website performance.",
+
       ],
     },
     {
-      company: "Bankit",
-      position: "Mobile Developer",
-      startDate: "Feb 2017",
-      endDate: "May 2018",
+      company: "The Home Depot",
+      position: "Email Marketing Automation Developer",
+      startDate: "Feb 2024",
+      endDate: "Current",
       summary: [
-        "I designed and developed a mobile application using Flutter, allowing it to be deployed on both Android and iOS with a single codebase. This reduced development time by 50% and maintenance costs by 30%, facilitating a consistent user experience on both platforms.",
-        "I integrated biometric authentication and data encryption, significantly improving the security of user data. This implementation resulted in a 40% increase in user trust and a 25% reduction in unauthorized access attempts.",
+        " Quality Assurance: Validating email campaigns for cross-platform functionality, visual consistency, and alignment with industry standards.  ",
+        " Performance Optimization: Analyzing engagement metrics, segmentation strategies, and personalization techniques to refine campaign effectiveness.  ",
+        " Automation & Integration: Developing tools, scripts, and workflows to simplify processes and enhance operational agility. ",
+        " Using a blend of HTML, CSS, JavaScript, Python, and tools such as Movable Ink, MessageGears, Jenkins, Google Cloud, and BigQuery, we work alongside analysts, engineers, QA teams, and stakeholders to innovate and scale The Home Depot’s email marketing initiatives.",
+
+
       ],
     },
     {
-      company: "Driveer",
-      position: "Frontend Developer",
-      startDate: "Jun 2015",
-      endDate: "Oct 2016",
+      company: "North American Hand Paper Makers",
+      position: "Volunteer Data Analyst",
+      startDate: "April 2024",
+      endDate: "Dec 2024",
       summary:
-        "Developed and integrated a real-time vehicle tracking system using WebSockets, improving accuracy and data update in the application. This functionality increased user satisfaction by 30% and reduced customer service inquiries by 25%.",
+        "Analyze large datasets using Python, Pandas, and NumPy to derive actionable insights. Use Streamlit as a reporting tool to create dynamic dashboards for data visualization and trend analysis. Collaborate with cross-functional teams to improve decision-making processes through data-driven reports.",
     },
   ],
   projects: [
     {
-      name: "Spotifu Music",
-      summary: "A music streaming app that emulates Spotify's core features.",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/spotifu.png",
+      name: "Hard Hat VR website",
+      summary: "Static website using WordPress for a VR training company. Using xAPI services to track user interactions within the VR training modules.",
+      linkPreview: "https://hardhatvr.com/",
+      //linkSource: '',
+      image: "/hhvr.png",
     },
     {
-      name: "Shopp App",
-      summary: "An e-commerce platform that replicates Shopify's key features.",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/shopify-clon.png",
+      name: "Data Analysis Dashboard",
+      summary: "An streamlit dashboard that analyzes sales data to provide insights and visualizations for better decision-making.",
+      linkPreview: "https://nv8yvteodjgps2k6zttase.streamlit.app/",
+      //linkSource: "",
+      image: "/da.png",
     },
     {
-      name: "ClonTagram",
-      summary: "A social network that replicates the features of Instagram",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/clone-ig.png",
+      name: "PMO ADVISORY Website",
+      summary: "A corporate website built with WordPress, featuring custom themes and plugins to enhance user experience and functionality.",
+      linkPreview: "https://pmoadvisory.com/",
+      //linkSource: "https://pmoadvisory.com/",
+      image: "/pmo.png",
     },
   ],
   about: {
     description: `
-      Hi, I’m Alejandro Múnez, a passionate Mobile and Web Developer with a knack for crafting seamless digital experiences. With a strong background in both Android and iOS development, as well as front-end web technologies, I thrive in the intersection where creativity meets technology.
-
-      Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
+      Hi, I’m Henry Larreal, a passionate Data Scientist and Web Developer with a knack for merging analytical insights with digital solutions. With a strong background in Python and data visualization, as well as front-end web technologies, I thrive in the intersection where data meets design. Over the years, I’ve honed my skills in crafting dynamic dashboards and responsive websites that not only solve complex problems but also drive user engagement. My projects range from optimizing site performance to building data-driven reports, all with a focus on efficiency, clarity, and value.
     `,
-    image: "/alejandro-big.jpg",
+    image: "/henry.png",
   },
 };
 
